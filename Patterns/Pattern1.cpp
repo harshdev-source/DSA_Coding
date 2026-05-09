ce6d52baf09885d8cf556392f7c2a55e8da2677c
harshdev-source <harshverma2404@gmail.com>
2026-05-09 15:56:45 +0530

    2nd Commit

---
+++ Patterns/Pattern1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Outer Loop
+    for(int i=1; i<=4 ; i++){
+        // Inner Loop
+        for(int j=1; j<=4; j++){
+            // Work
+            cout << i << " ";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}
+

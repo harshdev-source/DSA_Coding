8e5aed2305811261ff5d1be2722efbf8622ec9f7
harsh-verse <harsh.verma0240@gmail.com>
2026-09-15 19:32:24 +0530

    Vectors completed

---
+++ Bitwise/bitwise.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Bitwise operators 
+
+    cout << (3 & 5) << endl;
+    cout << (3 | 5) << endl;
+    cout << (3 ^ 5) << endl;
+
+    cout << (~6) <<endl;
+
+
+    return 0;
+}

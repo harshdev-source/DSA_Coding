c10ac8a520a15dfa6644019c0c1a4430a7d7e40e
harsh-verse <harsh.verma0240@gmail.com>
2026-09-17 22:46:13 +0530

    OOPs till deep copy constructor

--- OOPs/copyConstructor.cpp
+++ OOPs/copyConstructor.cpp
@@ -6,16 +6,22 @@ class Car{
     public:
         string name;
         string color;
+        int *mileage;
     
     Car(string name, string color){
         this->name=name;
         this->color=color;
+        mileage = new int;  //Dynamic allocation
+        *mileage = 12;
     }
 
     Car(Car &original){
         cout << "Copying original to new.. \n";
         name = original.name;
         color = original.color;
+        mileage = new int;
+        *mileage = *original.mileage;
+
     }
 };
 
@@ -25,8 +31,16 @@ int main(){
     Car c1("Maruti 800", "White");
     
     Car c2(c1); // Custom copy constructor
+ 
     cout << c2.name << endl;
     cout << c2.color << endl;
+    cout << *c2.mileage << endl;
+    
+    *c2.mileage = 18;
+    cout << *c2.mileage << endl;
+
+    
+    cout << *c1.mileage << endl;
     
     return 0;
 }
